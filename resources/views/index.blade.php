cc466dc93f2532bfc53a080c71c7cdbdfc45fd16
Ali Karabay <alikarabay77@gmail.com>
2022-01-28 14:36:48 +0300

    logo added.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,8 +15,51 @@
     <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
-
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/images/logo-mini.jpg')}}">
     <link rel="stylesheet" href="{{asset('assets/css/aos.css')}}">
+
+
+    <!-- Android  -->
+    <meta name="theme-color" content="red">
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <!-- iOS -->
+    <meta name="apple-mobile-web-app-title" content="Application Title">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+    <!-- Windows  -->
+    <meta name="msapplication-navbutton-color" content="red">
+    <meta name="msapplication-TileColor" content="red">
+    <meta name="msapplication-TileImage" content="{{asset('assets/images/logo-mini.jpg')}}">
+    <meta name="msapplication-config" content="browserconfig.xml">
+
+    <!-- Pinned Sites  -->
+    <meta name="application-name" content="Application Name">
+    <meta name="msapplication-tooltip" content="Tooltip Text">
+    <meta name="msapplication-starturl" content="/">
+
+    <!-- Tap highlighting  -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <!-- UC Mobile Browser  -->
+    <meta name="full-screen" content="yes">
+    <meta name="browsermode" content="application">
+
+    <!-- Disable night mode for this page  -->
+    <meta name="nightmode" content="enable/disable">
+
+    <!-- Fitscreen  -->
+    <meta name="viewport" content="uc-fitscreen=yes"/>
+
+    <!-- Layout mode -->
+    <meta name="layoutmode" content="fitscreen/standard">
+
+    <!-- imagemode - show image even in text only mode  -->
+    <meta name="imagemode" content="force">
+
+    <!-- Orientation  -->
+    <meta name="screen-orientation" content="portrait">
 
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
