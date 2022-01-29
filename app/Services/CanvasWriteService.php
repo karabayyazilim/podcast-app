c4f4f6c075393fc7a3ed1750095f00569ae57da6
Ali Karabay <alikarabay77@gmail.com>
2022-01-29 17:10:45 +0300

    CanvasWriteService font change and gd module changed.

--- app/Services/CanvasWriteService.php
+++ app/Services/CanvasWriteService.php
@@ -13,15 +13,16 @@ class CanvasWriteService
 
         $img = Image::make(public_path('assets/images/canvas.jpg'));
         $img->text($text, 252.5, 255.5, function($font) {
-            $font->file(public_path('assets/fonts/ubuntu/Ubuntu-Medium.ttf'));
+            $font->file(public_path('assets/fonts/resagnicto/ResagnictoBold.ttf'));
             $font->size(28);
             $font->color('#000');
             $font->align('center');
             $font->valign('bottom');
+            $font->kerning(5);
             $font->angle(0);
         });
         $img->text($description, 252.5, 300, function($font) {
-            $font->file(public_path('assets/fonts/ubuntu/Ubuntu-Medium.ttf'));
+            $font->file(public_path('assets/fonts/resagnicto/ResagnictoBold.ttf'));
             $font->size(18);
             $font->color('#000');
             $font->align('center');
