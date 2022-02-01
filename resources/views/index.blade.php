6e0e4e29bdf69566c98b79b1f30c50b9d6f61ab6
Ali Karabay <alikarabay77@gmail.com>
2022-02-01 18:31:43 +0300

    google analytics added.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,6 +28,16 @@
 
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128682056-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-128682056-1');
+    </script>
+
     <style>
         h3 {
             color: #50a5a0;
